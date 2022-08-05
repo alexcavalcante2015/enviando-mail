26de6461000d186c7c6227075dcd2d3ee51da4df
Alex <Alex@DESKTOP-5DKHRF1>
2022-08-05 07:39:40 -0300

    Fim AULA 19.1 - Criando o projeto JavaMail com Maven

--- enviando-mail/src/test/java/enviando/email/AppTest.java
+++ enviando-mail/src/test/java/enviando/email/AppTest.java
@@ -1,38 +1,15 @@
 package enviando.email;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+   
+	@org.junit.Test
+	public void testeEmail() {
+		
+		/*Olhe as configurações smtp do seu email*/
+		
+	}
+	
 }
