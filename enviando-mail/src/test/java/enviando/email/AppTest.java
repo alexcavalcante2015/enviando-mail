2e10de72ef5a161cb57fcce6ee9cd0f9770fb0c4
Alex <Alex@DESKTOP-5DKHRF1>
2022-08-06 08:01:10 -0300

    Fim AULA 19.7 - Criando a classe ObjetoEnviaEmail para reutilizar

--- enviando-mail/src/test/java/enviando/email/AppTest.java
+++ enviando-mail/src/test/java/enviando/email/AppTest.java
@@ -16,48 +16,25 @@ import javax.mail.internet.MimeMessage;
  */
 public class AppTest {
 	
-	private String userName = "alexcomte524@gmail.com";
-	private String senha = "wbxdcoayrwczauiv";
+	
    
 	@org.junit.Test
-	public void testeEmail() {
-		try {
+	public void testeEmail() throws Exception{
 		
-		Properties properties = new Properties();
+		ObjetoEnviaEmail enviaEmail = 
+				new ObjetoEnviaEmail("alexcavalcante2015@gmail.com, mastodonalex2@gmail.com", 
+									"Alex do JDev Treinamento",
+									"Testando e-mail com Java", 
+									"Esse texto é a descrição do meu email");
 		
-		properties.put("mail.smtp.ssl.trust", "*");
-		properties.put("mail.smtp.auth", "true"); /*Autorização*/
-		properties.put("mail.smtp.starttls", "true"); /*Autenticação*/
-		properties.put("mail.smtp.host", "smtp.gmail.com"); /*Servidor gmail Google*/
-		properties.put("mail.smtp.port", "465"); /*Porta do servidor*/
-		properties.put("mail.smtp.socketFactory.port", "465"); /*Especifica a porta a ser conectada pelo socket*/
-		properties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory"); /*Classe socket de conexão ao SMTP*/
-		
-		Session session = Session.getInstance(properties, new Authenticator() {
-			@Override
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(userName, senha);
-			}
-		});
-		
-		Address[] toUser = InternetAddress.parse("alexcavalcante2015@gmail.com, mastodonalex2@gmail.com, alexcomte524@gmail.com");
-		
-		Message message = new MimeMessage(session);
-		message.setFrom(new InternetAddress(userName, "Alex JDev - Treinamento")); /*Quem está enviando*/
-		message.setRecipients(Message.RecipientType.TO, toUser); /*Email de destino*/
-		message.setSubject("Chegou o e-mail enviado com Java"); /*Assunto do e-mail*/
-		message.setText("Olá programador, você acaba de receber um e-mail enviado com Java do curso Formação Java Web do Alex"); /*Corpo do e-email*/
-		
-		Transport.send(message);
+		enviaEmail.enviarEmail();
 		
 		/*Caso o email não esteja sendo enviado então
 		 * coloque um tempo de espera mais isso só pode
 		 * usado para testes*/
 		Thread.sleep(5000);
 		
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		
 	}
 	
 }
